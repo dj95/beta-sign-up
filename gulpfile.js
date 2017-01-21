6373d0990632dbd62dcb5b89cbabb5ac671611d3
Raphael Beer <raphael.beer@protonmail.com>
2017-01-21 23:40:46 +0100

    Meta: Fix gulpfile, package.json; organize styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,54 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var pkg = require('./package.json');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const header = require('gulp-header');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const maps = require('gulp-sourcemaps');
+const clean = require('gulp-clean');
+const pkg = require('./package.json');
 
 // Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
-    ' */\n',
-    ''
+const banner = ['/*!\n',
+  ' * ETdb  - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2016-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+  ' */\n',
+  ''
 ].join('');
 
-// Compile LESS files from /less into /css
-//gulp.task('less', function() {
-//    return gulp.src('less/agency.less')
-//        .pipe(less())
-//        .pipe(header(banner, { pkg: pkg }))
-//        .pipe(gulp.dest('css'))
-//        .pipe(browserSync.reload({
-//            stream: true
-//        }))
-//});
+gulp.task('clean', () => gulp.src('css/dist', { read: false }).pipe(clean()));
+
+gulp.task('sass', () => gulp.src('styles/scss/*.scss')
+  .pipe(maps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(header(banner, { pkg: pkg }))
+  .pipe(maps.write('../maps'))
+  .pipe(gulp.dest('styles/css')));
 
 // Minify compiled CSS
-gulp.task('minify-css', ['sass'], function() {
-    return gulp.src('css/agency.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
-
-// Minify JS
-gulp.task('minify-js', function() {
-    return gulp.src('js/agency.js')
-        .pipe(uglify())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('js'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
+gulp.task('minify-css', () => gulp.src('styles/css/*.css')
+  .pipe(maps.init())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(maps.write('../maps'))
+  .pipe(gulp.dest('styles/dist'))
+  .pipe(browserSync.reload({
+    stream: true
+  })));
 
 // Run everything
-gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default', ['clean', 'sass', 'minify-css']);
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: ''
-        },
-        ui: {
-            port: 8081
-        },
-        port: 8080
-    })
-})
+gulp.task('browserSync', () => browserSync.init({
+  server: { baseDir: '' },
+  ui: { port: 8081 },
+  port: 8080
+}));
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
-    // gulp.watch('less/*.less', ['less']);
-    gulp.watch('css/*.css', ['minify-css']);
-    gulp.watch('js/*.js', ['minify-js']);
+gulp.task('dev', ['browserSync', 'default'], () => {
+    gulp.watch('styles/scss/*.scss', ['sass']);
+    gulp.watch('styles/css/*.css', ['minify-css']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
